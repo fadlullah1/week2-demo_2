5aa9411a21f9c98b992c0a2896df2a9bbfb9eff5
Saliu Fadlullahgit config --global user.name Saliu <l00179048@atu.iel00179048@atu.iegit config --global user.email l00179048@atu.ie>
2024-03-19 15:29:27 +0000

    commit2

--- demo2.R
+++ demo2.R
@@ -41,3 +41,84 @@ q5 <- c(5,5,2,NA,1)
 Manager <- data.frame(manager,date,country,gender,age,q1,q2,q3,q4,q5)
 Manager
 View(Manager)
+
+Manager$age[Manager$manager==5]<-NA
+Manager
+
+
+# add new column
+Manager$agecat <- c(10,22,33,44,50)
+Manager
+
+
+#drop column
+Managers<- subset(Manager, select = -manager)
+Managers
+
+Managers <- subset(Manager, select = -1)
+Managers
+
+Manager1 <- Manager[,c(2:11)]
+Manager1
+
+#view first 4 rows
+Manager2<- Manager[c(1:4),]
+Manager2
+
+Managers [1:3,1:3]
+
+Managers [c(1,3),1:3]
+
+Managers[c(1,3,5),c(2,4)]
+
+#structure of the data frame
+str(Managers)
+
+Managers <-subset(Managers,select = -10)
+Managers
+
+drop(agecat)
+agecat
+#categorizing all managers by age category
+Managers$agecat [Managers$age >= 45] <- "elder"
+Managers$agecat [Managers$age <= 25] <- "young"
+Managers$agecat [Managers$age >26 & Managers$age < 44] <- "middleage"
+Managers$agecat [is.na(Managers$age)] <- "elder"
+Managers
+
+Managers$agecat <- factor(Managers$agecat, order = TRUE, levels = c("young","middleage","elder"))
+Managers
+
+str(agecat)
+
+Managers$agecat <- agecat
+
+str(Managers)
+
+summary_col <- Managers$q1 + Managers$q2 + Managers$q3 + Managers$q4 + Managers$q5
+summary_col
+
+Managers$summary_col <- summary_col
+Managers
+
+Managers <- data.frame(Managers,summary_col)
+Managers
+
+#drop 
+Managers<- subset(Managers, select = -12)
+Managers
+
+mean_value <- rowMeans(Managers[5:9])
+mean_value
+
+Managers$mean_value <- mean_value
+Managers
+
+#change the names of column 
+names(Managers)[12] <- "mean value"
+
+names(Managers)[11] <- "total "
+Managers
+
+col_summary <- colSums(Managers[,c(4:9,11:12)],na.rm = TRUE)
+col_summary
